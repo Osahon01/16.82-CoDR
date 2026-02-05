d270789344e1208b2a90399f99912b492c42011a
opera <ethan26@mit.edu>
2026-02-04 22:49:14 -0500

    Add cpp optimizer

---
+++ 82_cpp_opt/82opt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <chrono>
+
+#include <pagmo/algorithm.hpp>
+
+#include <pagmo/algorithms/sade.hpp>
+#include <pagmo/archipelago.hpp>
+#include <pagmo/problem.hpp>
+#include <pagmo/algorithms/gaco.hpp>
+#include <pagmo/population.hpp>
+
+int main(){
+    return 0;
+}
