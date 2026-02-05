04bd3aa4359e3733ce7a90c96808cb9f87ad84ec
opera <ethan26@mit.edu>
2026-02-04 23:39:23 -0500

    Cleanup fitness function

--- 82_cpp_opt/fitness.cpp
+++ 82_cpp_opt/fitness.cpp
@@ -16,18 +16,6 @@ vector_double::size_type problem_fvd::get_nic() const{
 }
 
 std::pair<vector_double, vector_double> problem_fvd::get_bounds() const{
-    // auto omega = x[0]; // rad/s - shaft speed
-    // auto u_i = x[1];   // m/s   - compressor inlet velocity
-    // auto T_4 = x[2];   // K     - combustor exit total temp
-    // auto R_Cih = x[3]; // m     - compressor inlet hub radius
-    // auto R_Cit = x[4]; // m     - compressor inlet tip radius
-    // auto A_Co = x[5];  // m^2   - compressor outlet area
-    // auto R_Com = x[6]; // m     - compressor outlet meanline radius
-    // auto D_T_C = x[7]; // K     - compressor total temperature change
-    // auto R_Tih = x[8]; // m     - turbine inlet hub radius
-    // auto R_Tit = x[9]; // m     - Turbine inlet tip radius
-    // auto A_To = x[10]; // m^2   - Turbine outlet area
-    // auto R_Tom = x[11];// m     - Turbine exit meanline radius
     vector_double lb = {};
     vector_double ub = {};
     std::pair<vector_double, vector_double> ret(lb,ub);
