46bddcaf6f9e77d2af4304152da2a00c788e30b9
Biruk <israelbiruk10gmail.com>
2026-02-13 17:08:59 -0500

    Wrote function to calculate parastic drag

--- 82_py/cruise_drag_model.py
+++ 82_py/cruise_drag_model.py
@@ -2,52 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ambiance import Atmosphere
 
-# TODO: find parastic drag through wind tunnel test data 
-
-# Constants
+# (Assumed) Constants
 pi = np.pi
 AR = 9.702
 e = 0.7
-CD0 = 0.032
 
 # Aircraft parameters (Caravan-like)
 W = 39000        # N
 S = 25.96        # m^2
 h = 3000         # meters (~10,000 ft)
 
-# Atmosphere
+# Density as a function of atmosphere
 atm = Atmosphere(h)
 rho = atm.density[0]
 
 # Velocity sweep (m/s)
-V = np.linspace(40, 120, 500) #Note: Cessna Caravan has cruise speed of about 96 m/s
-
+V = np.linspace(40, 120, 500) # Note: Cessna Caravan has cruise speed of about 96 m/s
 q = 0.5 * rho * V**2
+CL = W / (q * S) # Required CL for level flight
 
-# Required CL for level flight
-CL = W / (q * S)
 
-# Drag components
+# Caravan Parastic Drag Calc
+def parastic_drag():
+    S_caravan = 25.96
+    AR_caravan = 9.702
+    P_caravan = 503e3 # W
+    eta_prop = 0.8 # assumed
+    V_cruise_caravan = 96 # m/s
+    D_caravan = P_caravan * eta_prop / V_cruise_caravan
+    q_caravan = 1/2 * rho * V_cruise_caravan**2
+
+    Cl_total_caravan = 35600/ (q_caravan*S_caravan)
+    Cd_total_caravan = D_caravan / (q_caravan * S_caravan)
+    CD_i_caravan = Cl_total_caravan**2 / (pi*AR_caravan*e)
+    CD0 = Cd_total_caravan - CD_i_caravan
+    return CD0
+CD0 = parastic_drag()
+
+
+# Our Drag components
 CDi = CL**2 / (pi * AR * e)
-
 D_i = q * S * CDi
 D_p = q * S * CD0
-
 D_total = D_i + D_p
 
-# Plot
-plt.figure(figsize=(8,6))
-plt.plot(V, D_i, label="Induced Drag")
-plt.plot(V, D_p, label="Parasite Drag")
-plt.plot(V, D_total, label="Total Drag", linewidth=2)
-plt.xlabel("Velocity (m/s)")
-plt.ylabel("Drag (N)")
-plt.title("Drag vs Velocity")
-plt.legend()
-plt.grid()
-plt.show()
 
-# Find minimum drag speed
-min_index = np.argmin(D_total)
-print("Velocity at Minimum Drag:", V[min_index], "m/s")
+# Plot
+PLOT = False
+if PLOT:
+    plt.figure(figsize=(8,6))
+    plt.plot(V, D_i, label="Induced Drag")
+    plt.plot(V, D_p, label="Parasite Drag")
+    plt.plot(V, D_total, label="Total Drag", linewidth=2)
+    plt.xlabel("Velocity (m/s)")
+    plt.ylabel("Drag (N)")
+    plt.title("Drag vs Velocity")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+    # Find minimum drag speed
+    min_index = np.argmin(D_total)
+    print("Velocity at Minimum Drag:", V[min_index], "m/s")
 
