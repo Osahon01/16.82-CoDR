f62c0ce0583fbdea8156c7d6e45338197e8a6753
opera <ethan26@mit.edu>
2026-02-16 16:54:24 -0500

    Add cpp optimization vars and surrogate models for blown lift condition

--- 82_cpp_opt/fitness.cpp
+++ 82_cpp_opt/fitness.cpp
@@ -36,6 +36,15 @@ vector_double problem_fvd::fitness(const vector_double &x) const{
         //     return ret;
         // }
 
+        auto m = x[0];
+        auto f = x[1];
+        auto S = x[2];
+        auto AR = x[3];
+        auto T_TO = x[4];
+        auto alpha_TO = x[5];
+        auto h_d = x[6];
+        auto delta_F = x[7];
+
         // Package objective and constraint values
         vector_double ret = {};
         // A physically impossible situation will usually result in a bunch of NaNs,
@@ -53,6 +62,28 @@ vector_double problem_fvd::fitness(const vector_double &x) const{
     }
 }
 
+double problem_fvd::blown_Cl_surr(double alpha, double Delta_CJ, double Ad_S, double delta_F) const{
+    auto delta_J = delta_J_surr(Ad_S, delta_F);
+    double CL = X1 + X2*delta_J*X3*alpha
+                + std::pow(Delta_CJ,0.5)*(X4*delta_J+X5*alpha*X6)
+                + Delta_CJ*(X7*delta_J+X8*alpha*X9);
+    return CL;
+}
+
+double problem_fvd::delta_J_surr(double Ad_S, double delta_F) const{
+    //Surrogate model for flap efficiency as a function of flap deflection and 
+    //Nondimensional jet wash height
+    double eta_40 = 1./(f1_40*std::exp(f2_40*(Ad_S))+f3_40*std::exp(f4_40*(Ad_S)));
+    double eta_60 = 1./(f1_60*std::exp(f2_60*(Ad_S))+f3_60*std::exp(f4_60*(Ad_S)));
+    double k_b = delta_F/20. - 2.;
+    if (delta_F<40){
+        return eta_40;
+    }
+    if (delta_F>60){
+        return eta_60;
+    }
+    return eta_40 + k_b*(eta_60 - eta_40);
+}
 
 // Checks if anything in a vector_double is inf or nan
 bool problem_fvd::invalid_ret(vector_double &x) const{
